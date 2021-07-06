df83523f6f09e0cf4a4702f632d6f20e98bbcb77
Joséphine Calandra <jmo.calandra@gmail.com>
2021-07-06 14:05:50 +0200

    clean code

--- src/oracle_version/oracle_mso.py
+++ src/oracle_version/oracle_mso.py
@@ -727,7 +727,6 @@ class MO(FactorOracle):
                 self.rep.append([new_data, 1])
                 if len(self.rep) > 1:
                     self.vec.append(comp_rep)
-                    print("in mso vec", self.vec)
 
             else:  # Si on a un matériau déjà existant
                 # trie avec le 2e élément de chaque couple par ordre décroissant
@@ -753,10 +752,8 @@ class MO(FactorOracle):
                 self.latent.append([i])
                 self.data.append(len(self.latent) - 1)
                 self.rep.append([new_data, 1])
-                print("in mso comp rep", comp_rep)
                 if len(self.rep) > 1:
                     self.vec.append(comp_rep)
-                    print(self.vec)
             else:
                 self.sfx[i] = suffix_candidate
                 # self.lrs[i] = self._len_common_suffix(pi_1, self.sfx[i] - 1) + 1
