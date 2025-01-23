76447ecad9cd60ff8bc8b5d23522cb731ab38ee2
Joséphine Calandra <jmoca@MacBook-Pro.local>
2025-01-23 16:59:06 +0100

    add durations attribute to concatObjects

--- src/version2/core/module_mso/mso/class_concatObj.py
+++ src/version2/core/module_mso/mso/class_concatObj.py
@@ -3,23 +3,28 @@ from class_object import Descriptors, borders
 class ConcatObj:
     def __init__(self):
         self.objects = []
-        self.concat_signal = []
-        self.descriptors = Descriptors()
+        self.concat_signals = []
         self.concat_labels = ""
+        self.descriptors = Descriptors()
+        self.durations = 0
         self.size = 0
 
     def init(self, new_obj):
         self.objects = [new_obj]
-        self.concat_signal = new_obj.signal
-        self.descriptors.copy(new_obj.descriptors)
+        self.concat_signals = new_obj.signal
         self.concat_labels = new_obj.label
+        self.descriptors.copy(new_obj.descriptors)
+        self.durations = new_obj.duration
         self.size = 1
 
     def _update_objects(self, obj):
         self.objects.append(obj)
 
     def _update_signal(self, signal):
-        self.concat_signal += signal
+        self.concat_signals += signal
+
+    def _update_labels(self, label):
+        self.concat_labels += label
 
     def _update_descriptors(self, new_descriptors):
         for i in range(self.descriptors.nb_descriptors):
@@ -29,18 +34,19 @@ class ConcatObj:
                 for k in range(len(new_descriptors.mean_descriptors[i][j])):
                     self.descriptors.mean_descriptors[i][j][k] = (self.descriptors.mean_descriptors[i][j][k]*self.size + new_descriptors.mean_descriptors[i][j][k])/(self.size+1)
 
-    def _update_labels(self, label):
-        self.concat_labels += label
+    def update_duration(self, duration):
+        self.durations += duration
+
+    def update_size(self):
+        self.size += 1
 
     def update(self, obj):
         self._update_objects(obj)
         self._update_signal(obj.signal)
-        self._update_descriptors(obj.descriptors)
         self._update_labels(obj.label)
-        self.size += 1
-
-    def _reset_concat_label(self, labels):
-        self.concat_labels = labels
+        self._update_descriptors(obj.descriptors)
+        self.update_duration(obj.duration)
+        self.update_size()
 
     def _reset_object(self, objects):
         self.objects = objects
@@ -48,30 +54,39 @@ class ConcatObj:
     def _reset_concat_signal(self, signals):
         self.concat_signal = signals
 
+    def _reset_concat_label(self, labels):
+        self.concat_labels = labels
+
     def _reset_descriptors(self, descriptors):
         self.descriptors.copy(descriptors)
 
+    def _reset_durations(self, durations):
+        self.durations = durations
+
     def reset(self, objects):
-        labels = objects[0].label
         signals = objects[0].signal
+        labels = objects[0].label
         descriptors = Descriptors()
         descriptors.copy(objects[0].descriptors)
+        durations = objects[0].duration
         for obj in objects[1:]:
             labels += obj.label
             signals.extend(obj.signal)
             for i in range(descriptors.nb_descriptors):
                 descriptors.update(obj.descriptors.concat_descriptors[i], obj.descriptors.mean_descriptors[i])
-        self._reset_concat_label(labels)
+            durations += obj.duration
         self._reset_object(objects)
         self._reset_concat_signal(signals)
+        self._reset_concat_label(labels)
         self._reset_descriptors(descriptors)
+        self._reset_durations(durations)
         self.size = len(labels)
 
     def _pop_concat_label(self):
         self.concat_labels = self.concat_labels[:-1]
 
     def _pop_concat_signal(self):
-        nb = int(len(self.concat_signal)/self.size)
+        nb = int(len(self.objects[-1].signal))
         self.concat_signal = self.concat_signal[:-nb]
 
     def _pop_object(self):
@@ -95,17 +110,25 @@ class ConcatObj:
             for i in range(self.descriptors.nb_descriptors):
                 self.descriptors.concat_descriptors[i].pop()
 
+    def _pop_duration(self):
+        self.durations = self.durations - self.objects[-1].duration
+
+    def _pop_size(self):
+        self.size = self.size - 1
+
     def pop(self):
         self._pop_concat_label()
         self._pop_concat_signal()
         self._pop_descriptors()
+        self._pop_duration()
         self._pop_object()
-        self.size = self.size - 1
+        self._pop_size()
 
     def copy(self, concatObj):
         self.objects = concatObj.objects
         self.concat_signal = concatObj.concat_signal
-        self.descriptors = concatObj.descriptors
         self.concat_labels = concatObj.concat_labels
+        self.descriptors = concatObj.descriptors
+        self.durations = concatObj.durations
         self.size = concatObj.size
 
